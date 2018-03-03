5a104c2c202e145a5ede797b1548174cacba3820
Jerome Kelleher <jk@well.ox.ac.uk>
2018-03-03 18:32:47 +0000

    Added converter for msprime HDF5.

--- file-format-experiment.py
+++ file-format-experiment.py
@@ -1,8 +1,13 @@
 """
 Experimental work for a simple key->numeric array store.
 """
-import numpy as np
 import struct
+import os.path
+import argparse
+
+import numpy as np
+import humanize
+import msprime
 
 
 # This is a rubbish magic number. Just generate something randomly for the
@@ -16,7 +21,8 @@ np_dtype_to_type_map = {
     # Obviously support more of these...
     "int8": 1,
     "uint32": 2,
-    "float64": 3,
+    "int32": 3,
+    "float64": 4,
 }
 
 type_to_np_dtype_map = {t: dtype for dtype, t in np_dtype_to_type_map.items()}
@@ -91,11 +97,15 @@ def write_arrays(filename, arrays):
         f.write(header)
 
         # We store the keys in sorted order.
+        # TODO Change this so that we write all the keys in one block and
+        # all the arrays afterwards. This is to allow us to efficiently read
+        # in the names of the arrays without seeking all over the file.
         sorted_keys = sorted(arrays.keys())
         descriptor_block_size = num_items * ItemDescriptor.size
         offset = header_size + descriptor_block_size
         descriptors = []
-        for key, array in arrays.items():
+        for key in sorted_keys:
+            array = arrays[key]
             assert len(array.shape) == 1  # Only 1D arrays supported.
             key_start = offset
             array_start = key_start + len(key) # TODO Add padding to 8-align
@@ -120,39 +130,51 @@ def write_arrays(filename, arrays):
             f.write(descriptor.array.data)
 
 
+def read_header(f):
+    """
+    Reads the header from the specified stream and returns the list
+    of descriptors.
+    """
+    header_size = 64
+    header = f.read(header_size)
+    if header[0:8] != MAGIC:
+        raise ValueError("Incorrect file format")
+    version_major = struct.unpack("<I", header[8:12])[0]
+    version_minor = struct.unpack("<I", header[12:16])[0]
+    if version_major != VERSION_MAJOR:
+        raise ValueError("Incompatible major version")
+    num_items = struct.unpack("<I", header[16:20])[0]
+
+    descriptor_block_size = num_items * ItemDescriptor.size
+    descriptor_block = f.read(descriptor_block_size)
+
+    offset = 0
+    descriptors = []
+    for _ in range(num_items):
+        descriptor = ItemDescriptor.unpack(
+            descriptor_block[offset: offset + ItemDescriptor.size])
+        descriptors.append(descriptor)
+        offset += ItemDescriptor.size
+        f.seek(descriptor.key_start)
+        descriptor.key = f.read(descriptor.key_len).decode()
+    return descriptors
+
+
 def read_arrays(filename):
     """
     Reads arrays from the specified file and returns the resulting mapping.
     """
     with open(filename, "rb") as f:
-        header_size = 64
-        header = f.read(header_size)
-        if header[0:8] != MAGIC:
-            raise ValueError("Incorrect file format")
-        version_major = struct.unpack("<I", header[8:12])[0]
-        version_minor = struct.unpack("<I", header[12:16])[0]
-        if version_major != VERSION_MAJOR:
-            raise ValueError("Incompatible major version")
-        num_items = struct.unpack("<I", header[16:20])[0]
-
-        descriptor_block_size = num_items * ItemDescriptor.size
-        descriptor_block = f.read(descriptor_block_size)
-
-        offset = 0
-        descriptors = []
-        for _ in range(num_items):
-            descriptor = ItemDescriptor.unpack(
-                descriptor_block[offset: offset + ItemDescriptor.size])
-            descriptors.append(descriptor)
-            offset += ItemDescriptor.size
+        descriptors = read_header(f)
 
         items = {}
         for descriptor in descriptors:
-            # TODO change this to seek to the start addresses and therefore
-            # skip padding.
-            assert f.tell() == descriptor.key_start
-            descriptor.key = f.read(descriptor.key_len).decode()
-            assert f.tell() == descriptor.array_start
+            # TODO this should skip reading the keys, as this should be done as
+            # part of the header reading process.
+            # assert f.tell() == descriptor.key_start
+            # descriptor.key = f.read(descriptor.key_len).decode()
+            # assert f.tell() ==
+            f.seek(descriptor.array_start)
             dtype = type_to_np_dtype_map[descriptor.type]
             data = f.read(descriptor.array_len)
             descriptor.array = np.frombuffer(data, dtype=dtype)
@@ -160,24 +182,181 @@ def read_arrays(filename):
         return items
 
 
-def main():
+def print_summary(filename):
+    """
+    Prints out a summary of the specified kas file.
+    """
+    # Definitely want a class so that we can do this sort of stuff
+    # with more control.
+    with open(filename, "rb") as f:
+        descriptors = read_header(f)
+
+    print("Summary for", filename)
+    print("Total size = ", humanize.naturalsize(os.path.getsize(filename), binary=True))
+    print("Num arrays = ", len(descriptors))
+    key_width = max(len(descriptor.key) for descriptor in descriptors)
+    for descriptor in descriptors:
+        print(
+            "{:<{}}".format(descriptor.key, key_width), #str(descriptor),
+            humanize.naturalsize(descriptor.array_len, binary=True))
+
+
+
+def hdf5_to_kas(hdf5_file, kas_file):
+
+    ts = msprime.load(hdf5_file)
+    tables = ts.tables
+
     arrays = {
-        "one/two": np.arange(10, dtype=np.int8),
-        "three/four/five": np.zeros(20, dtype=np.uint32) + 1,
-        "six": np.zeros(5, dtype=np.float64) + 2,
+        "sequence_length": np.array([ts.sequence_length]),
+
+        "nodes/flags": tables.nodes.flags,
+        "nodes/time": tables.nodes.time,
+        "nodes/population": tables.nodes.population,
+        "nodes/metadata": tables.nodes.metadata,
+        "nodes/metadata_offset": tables.nodes.metadata_offset,
+
+        "edges/left": tables.edges.left,
+        "edges/right": tables.edges.right,
+        "edges/parent": tables.edges.parent,
+        "edges/child": tables.edges.child,
+
+        "migrations/left": tables.migrations.left,
+        "migrations/right": tables.migrations.right,
+        "migrations/time": tables.migrations.time,
+        "migrations/node": tables.migrations.node,
+        "migrations/source": tables.migrations.source,
+        "migrations/dest": tables.migrations.dest,
+
+        "sites/position": tables.sites.position,
+        "sites/ancestral_state": tables.sites.ancestral_state,
+        "sites/ancestral_state_offset": tables.sites.ancestral_state_offset,
+        "sites/metadata": tables.sites.metadata,
+        "sites/metadata_offset": tables.sites.metadata_offset,
+
+        "mutations/site": tables.mutations.site,
+        "mutations/node": tables.mutations.node,
+        "mutations/derived_state": tables.mutations.derived_state,
+        "mutations/derived_state_offset": tables.mutations.derived_state_offset,
+        "mutations/metadata": tables.mutations.metadata,
+        "mutations/metadata_offset": tables.mutations.metadata_offset,
+
+        "provenances/timestamp": tables.provenances.timestamp,
+        "provenances/timestamp_offset": tables.provenances.timestamp_offset,
+        "provenances/record": tables.provenances.record,
+        "provenances/record_offset": tables.provenances.record_offset,
     }
 
-    # Key-array-store
-    filename = "tmp.kas"
-    write_arrays(filename, arrays)
+    write_arrays(kas_file, arrays)
 
-    other = read_arrays(filename)
-    assert list(other.keys()) == list(arrays.keys())
-    print("read:")
-    for k in other.keys():
-        assert np.array_equal(arrays[k], other[k])
-        print(k, "->", other[k])
+def load_tree_sequence(filename):
+    """
+    Loads a tree sequence from the specified kas file.
+    """
+    items = read_arrays(filename)
+    sequence_length = items["sequence_length"][0]
 
+    nodes = msprime.NodeTable()
+    nodes.set_columns(
+        flags=items["nodes/flags"],
+        time=items["nodes/time"],
+        population=items["nodes/population"],
+        metadata=items["nodes/metadata"],
+        metadata_offset=items["nodes/metadata_offset"])
+
+    edges = msprime.EdgeTable()
+    edges.set_columns(
+        left=items["edges/left"],
+        right=items["edges/right"],
+        parent=items["edges/parent"],
+        child=items["edges/child"])
+
+    migrations = msprime.MigrationTable()
+    migrations.set_columns(
+        left=items["migrations/left"],
+        right=items["migrations/right"],
+        node=items["migrations/node"],
+        source=items["migrations/source"],
+        dest=items["migrations/dest"],
+        time=items["migrations/time"])
+
+    sites = msprime.SiteTable()
+    sites.set_columns(
+        position=items["sites/position"],
+        ancestral_state=items["sites/ancestral_state"],
+        ancestral_state_offset=items["sites/ancestral_state_offset"],
+        metadata=items["sites/metadata"],
+        metadata_offset=items["sites/metadata_offset"])
+
+    mutations = msprime.MutationTable()
+    mutations.set_columns(
+        site=items["mutations/site"],
+        node=items["mutations/node"],
+        derived_state=items["mutations/derived_state"],
+        derived_state_offset=items["mutations/derived_state_offset"],
+        metadata=items["mutations/metadata"],
+        metadata_offset=items["mutations/metadata_offset"])
+
+    provenances = msprime.ProvenanceTable()
+    provenances.set_columns(
+        timestamp=items["provenances/timestamp"],
+        timestamp_offset=items["provenances/timestamp_offset"],
+        record=items["provenances/record"],
+        record_offset=items["provenances/record_offset"])
+
+    return msprime.load_tables(
+        nodes=nodes, edges=edges, migrations=migrations, sites=sites,
+        mutations=mutations, provenances=provenances, sequence_length=sequence_length)
+
+
+def check_ts(ts1, ts2):
+    assert ts1.sequence_length == ts2.sequence_length
+    assert ts1.tables.nodes == ts2.tables.nodes
+    assert ts1.tables.edges == ts2.tables.edges
+    assert ts1.tables.migrations == ts2.tables.migrations
+    assert ts1.tables.sites == ts2.tables.sites
+    assert ts1.tables.mutations == ts2.tables.mutations
+    assert ts1.tables.provenances == ts2.tables.provenances
+
+def run_convert(args):
+
+    hdf5_to_kas(args.input, args.output)
+
+    ts = load_tree_sequence(args.output)
+    check_ts(ts, msprime.load(args.input))
+
+
+def run_ls(args):
+    print_summary(args.input)
+
+def main():
+    top_parser = argparse.ArgumentParser(
+        description="Toolkit for key-array-store files.")
+    top_parser.add_argument(
+        "-V", "--version", action='version',
+        version='%(prog)s {}'.format("0.0.1"))
+    # This is required to get uniform behaviour in Python2 and Python3
+    subparsers = top_parser.add_subparsers(dest="subcommand")
+    subparsers.required = True
+
+    parser = subparsers.add_parser(
+        "convert",
+        help="Convert msprime HDF5 file to kas.")
+    parser.add_argument("input",
+        help="Input msprime HDF5 file.")
+    parser.add_argument("output",
+        help="Output KAS file.")
+    parser.set_defaults(runner=run_convert)
+
+    parser = subparsers.add_parser(
+        "ls",
+        help="Lists the contents of the KAS file.")
+    parser.add_argument("input",
+        help="Input KAS file.")
+    parser.set_defaults(runner=run_ls)
+
+    args = top_parser.parse_args()
+    args.runner(args)
 
 
 if __name__ == "__main__":
